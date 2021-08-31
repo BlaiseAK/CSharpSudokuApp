daaf76cc97c19e5ec75646faadb0eeeb7c13a5ae
BlaiseAK <blaise.a.kress@gmail.com>
2021-08-31 06:51:51 -0400

    Initialized project, created SplitContainer and created model for object that will be stored in the Button.Tag for validation

---
+++ CSharpSudokuApp/CSharpSudokuApp/Model/ButtonTagData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSudokuApp.Model
+{
+    class ButtonTagData
+    {
+        public int xPos { get; }
+        public int yPos { get; }
+
+        public int value { get; set; }
+
+        ButtonTagData()
+        {
+            xPos = 0;
+            yPos = 0;
+        }
+
+        ButtonTagData(int x, int y)
+        {
+            xPos = x;
+            yPos = y;
+        }
+
+        ButtonTagData(int x, int y, int val)
+        {
+            xPos = x;
+            yPos = y;
+            value = val;
+        }
+    }
+}
