a49a6a7658ee8d70aad5e8d60371360a1084d311
BlaiseAK <blaise.a.kress@gmail.com>
2021-09-01 22:52:18 -0400

    Added Radio Buttons dynamically to second panel and used changed event to set the active number

--- CSharpSudokuApp/CSharpSudokuApp/Form1.cs
+++ CSharpSudokuApp/CSharpSudokuApp/Form1.cs
@@ -15,6 +15,35 @@ namespace CSharpSudokuApp
         public Form1()
         {
             InitializeComponent();
+            createRadioBtns();
+        }
+
+        public int activeNumber = 0;
+        
+        private void radioBtn_CheckedChanged(Object sender, EventArgs e)
+        {
+            RadioButton rBtn = sender as RadioButton;
+            activeNumber = int.Parse(rBtn.Text);
+        }
+
+        public void createRadioBtns()
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                RadioButton rBtn = new RadioButton()
+                {
+                    Text = $"{i}",
+                    Font = new Font(FontFamily.GenericSansSerif, 15),
+                    ForeColor = Color.Black,
+                    TextAlign = ContentAlignment.MiddleLeft,
+                    Name = $"RadioBtn{i}",
+                    Left = (i * 40),
+                    Top = 70,
+                    Size = new System.Drawing.Size(35, 40)
+                };
+                rBtn.CheckedChanged += radioBtn_CheckedChanged;
+                splitContainer1.Panel2.Controls.Add(rBtn);
+            }
         }
     }
 }
